a97812d54cabd8347aa3bf37964cbef00faf9323
clbokea <clbo@kea.dk>
2017-04-21 10:48:55 +0200

    exercises start

--- Code/BasicPrintStreamExample.java
+++ Code/BasicPrintStreamExample.java
@@ -1,6 +1,8 @@
 import java.io.PrintStream;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class BasicPrintStreamExample
@@ -8,16 +10,30 @@ public class BasicPrintStreamExample
     public static void main(String[] args)
     {
         try{
-            
-            // Scanner scan = new Scanner(new File("person.txt"));
+            // Læs fra text fil
+            Scanner scan = new Scanner(new File("person.txt"));
+            ArrayList<Person> persons = new ArrayList<Person>();
+ 
+            while(scan.hasNextLine())
+            {
+                persons.add(new Person(scan.next(), scan.next(), scan.nextInt(), scan.next()));   
+            }
 
+            // Skriver til text filen
             PrintStream file = new PrintStream(new File("person.txt"));
-        
-        
-        
-        } catch(FileNotFoundException e)
-        {
+            persons.add(new Person("Hans", "Olsen", 67, "221070-9977"));
+            
+            for(int i = 0; i < persons.size(); i++)
+            {
+                file.print(persons.get(i).getName() + " " + persons.get(i).getLastName() + " " + persons.get(i).getAge() + " " + persons.get(i).getCpr());
+            }
 
+                
+        
+        
+        } catch(Exception e)
+        {
+            System.out.println(e);
         }
        
     }
