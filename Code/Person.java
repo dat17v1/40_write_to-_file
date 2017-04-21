afa6baa53a4ef037f2d9119726393c04db7b2fc6
clbokea <clbo@kea.dk>
2017-04-21 08:26:45 +0200

    before start

---
+++ Code/Person.java
@@ -0,0 +1,29 @@
+public class Person
+{
+    private String name; 
+    private String lastName;
+    private int age;
+    private String cpr;
+
+    public Person(String name, String lastName, int age, String cpr)
+    {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.cpr = cpr;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getCpr(){
+        return cpr;
+    }
+}
